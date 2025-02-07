d0849274c193a55d42607c1e4a79456f96c74296
Peter Levinsky <pele@zealand.dk>
2025-02-07 10:18:55 +0100

    Add project files.

---
+++ ClassDemoTemplate/Worker.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassDemoTemplate
+{
+    public class Worker
+    {
+        public void Start()
+        {
+            List<String> data = new List<string>()
+            {
+                "Peter",
+                "Anders",
+                "Vibeke",
+                "Michael C"
+            };
+
+            AbstractTemplateClass temp = new therTSubTemplate();
+            temp.InsertTemplateMethod(data);
+
+            Console.WriteLine(temp);
+
+        }
+    }
+}
